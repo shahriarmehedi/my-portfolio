1b35829bfb8b6e026363d29d48b54e30fe83f017
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2021-12-02 03:49:46 +0600

    social link added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,8 +11,8 @@ const About = () => {
                 <div className=" flex items-center flex-col lg:flex-row w-11/12 mx-auto bg-2 py-10 px-3 lg:p-16 rounded-box lg:border-l-8 border-gray-400">
                     <div className="lg:w-1/3">
                         <img className="transform transition duration-300 hover:scale-110 h-60 mb-10 mx-auto lg:h-80 border-8 border-gray-700 rounded-full" src={my_photo} alt="" />
-                        <button className="px-4 transform transition duration-300 hover:scale-110 border-bottom border-b-2 border-green-400 text-3xl py-2 bg-gray-800 text-white rounded m-2"><i class="fab fa-github"></i></button>
-                        <button className="px-4 transform transition duration-300 hover:scale-110 border-bottom border-b-2 border-green-400 text-3xl py-2  bg-gray-800 text-white rounded m-2"><i class="fab fa-linkedin"></i></button>
+                        <a href="https://github.com/shahriarmehedi" target="_blank" rel="noreferrer" className="px-4 transform transition duration-300 hover:scale-110 border-bottom border-b-2 border-green-400 text-3xl py-2 bg-gray-800 text-white rounded m-2"><i class="fab fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/shahriarmehedi/" target="_blank" rel="noreferrer" className="px-4 transform transition duration-300 hover:scale-110 border-bottom border-b-2 border-green-400 text-3xl py-2  bg-gray-800 text-white rounded m-2"><i class="fab fa-linkedin"></i></a>
                     </div>
                     <div className="lg:w-2/3 text-center lg:text-left lg:pl-10">
                         <h1 className=" text-2xl lg:text-4xl  text-green-400 pt-10 lg:pt-0">Md. Shahriar Mehedi</h1>
